94cd0c31ba55626e2d736184715bc607eb24f87d
tnanko2019 <55319920+tnanko2019@users.noreply.github.com>
2020-11-29 01:05:18 +0800

    Create smartReplace.js

---
+++ smartReplace.js
@@ -0,0 +1,50 @@
+//https://github.com/sazs34/MyActions
+
+const download = require("download");
+async function replaceWithSecrets(content, Secrets) {
+    if (!Secrets || !Secrets) return content;
+    const replacements = [];
+    await init_notify(Secrets, content, replacements);
+        if (Secrets.COOKIE_QQYD) {
+            replacements.push({ key: "$.getdata(qqreadurlKey)", value: JSON.stringify(Secrets.COOKIE_QQYD.split("\n")[0]) });
+            replacements.push({ key: "$.getdata(qqreadheaderKey)", value: JSON.stringify(Secrets.COOKIE_QQYD.split("\n")[1]) });
+            replacements.push({ key: "$.getdata(qqreadtimeurlKey)", value: JSON.stringify(Secrets.COOKIE_QQYD.split("\n")[2]) });
+            replacements.push({ key: "$.getdata(qqreadtimeheaderKey)", value: JSON.stringify(Secrets.COOKIE_QQYD.split("\n")[3]) });
+        }
+        await downloader(content);//检查所需额外js
+    return batchReplace(content, replacements);
+}
+function batchReplace(content, replacements) {
+    for (var i = 0; i < replacements.length; i++) {
+        content = content.replace(replacements[i].key, replacements[i].value);
+    }
+    return content;
+}
+
+async function init_notify(Secrets, content, replacements) {
+    if (!Secrets.PUSH_KEY && !Secrets.BARK_PUSH && !Secrets.TG_BOT_TOKEN) {
+        if (content.indexOf("require('./sendNotify')") > 0) {
+            replacements.push({
+                key: "require('./sendNotify')",
+                value:
+                    "{sendNotify:function(){},serverNotify:function(){},BarkNotify:function(){},tgBotNotify:function(){}}",
+            });
+        }
+    }
+}
+async function downloader(content) {
+    if (content.indexOf("jdFruitShareCodes") > 0) {
+        await download_jdFruit();
+    }
+}
+
+async function download_notify() {
+    await download("https://github.com/lxk0301/scripts/raw/master/sendNotify.js", "./", {
+        filename: "sendNotify.js",
+    });
+    console.log("下载通知代码完毕");
+}
+
+module.exports = {
+    replaceWithSecrets,
+};
